1771465ccb68b8cddd16594f579d9c6610e46b92
Jordan Harrison <orrotmg@gmail.com>
2025-11-23 15:43:16 +1030

    Added initialisation for Enclosure

--- enclosure.py
+++ enclosure.py
@@ -8,5 +8,9 @@ This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
 class Enclosure:
-    def __init__(self):
-        pass
+    def __init__(self, name, environment_type, size, cleanliness_level):
+        self.name = name
+        self.environment_type = environment_type
+        self.size = size
+        self.cleanliness_level = cleanliness_level
+        self.animals = []
