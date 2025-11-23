8de32258a937905370499c7f1c01b195e712e8ae
Jordan Harrison <orrotmg@gmail.com>
2025-11-23 16:01:07 +1030

    Added validation

--- staff.py
+++ staff.py
@@ -1,6 +1,6 @@
 '''
 File: staff.py
-Description: 
+Description: Base class for zoo staff members.
 Author: Jordan Harrison
 ID: 110350998
 Username: harjd001
@@ -9,6 +9,11 @@ This is my own work as defined by the University's Academic Integrity Policy.
 
 class Staff:
     def __init__(self, name, role):
+        if not isinstance(name, str) or not name:
+            raise ValueError("Invalid name")
+        if not isinstance(role, str) or not role:
+            raise ValueError("Invalid role")
+
         self.name = name
         self.role = role
         self.assigned_animals = []
