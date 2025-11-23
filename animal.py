8c015fc2be3be429bea991fcf0cd1ceb2e32aa87
Jordan Harrison <orrotmg@gmail.com>
2025-11-23 15:46:08 +1030

    Added data validation to init

--- animal.py
+++ animal.py
@@ -9,6 +9,14 @@ This is my own work as defined by the University's Academic Integrity Policy.
 
 class Animal:
     def __init__(self, name, species, age, diet):
+        if not isinstance(name, str) or not name:
+            raise ValueError("Invalid name")
+        if not isinstance(species, str) or not species:
+            raise ValueError("Invalid species")
+        if not isinstance(age, int) or age < 0:
+            raise ValueError("Invalid age")
+        if not isinstance(diet, str) or not diet:
+            raise ValueError("Invalid diet")
         self.name = name
         self.species = species
         self.age = age
