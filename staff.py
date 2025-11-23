4b7ac03cbda2ed958c5648e288bfab8f86f7cb2c
Jordan Harrison <orrotmg@gmail.com>
2025-11-23 15:43:45 +1030

    Added initialisation for Staff

--- staff.py
+++ staff.py
@@ -8,5 +8,8 @@ This is my own work as defined by the University's Academic Integrity Policy.
 '''
 
 class Staff:
-    def __init__(self):
-        pass
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+        self.assigned_animals = []
+        self.assigned_enclosures = []
