3ac8985fd85a0cf6701aae0e7943f25784760118
Jordan Harrison <orrotmg@gmail.com>
2025-11-23 15:37:16 +1030

    Added headers to all files

--- enclosure.py
+++ enclosure.py
@@ -1,8 +1,8 @@
 '''
-File: filename.py
-Description: A brief description of this Python module.
-Author: Billy Bizilis
-ID: 110100110
-Username: bizvy001
+File: enclosure.py
+Description: 
+Author: Jordan Harrison
+ID: 110350998
+Username: harjd001
 This is my own work as defined by the University's Academic Integrity Policy.
-''
+'''
